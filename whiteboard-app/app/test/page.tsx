722b67a2185c1918df451c9ec79833031b0f9948
Mekhi-M <mekhim0731@gmail.com>
2024-08-18 16:17:58 -0400

    fin project structure setup + testing

--- whiteboard-app/app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-const Page = () => {
-	return <div>Hello Test Page</div>;
-};
-
-export default Page;
--- whiteboard-app/app/test/subroute/page.tsx
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-const Page = () => {
-	return <div>Hello Subroute Page</div>;
-};
-
-export default Page;
